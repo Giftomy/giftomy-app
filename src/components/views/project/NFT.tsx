72bdf4f74b7cbaf8b8176f20e71adfefc74e60fe
HA <>
2022-05-27 16:05:30 +0800

    refactor: code format

--- src/components/views/project/NFT.tsx
+++ src/components/views/project/NFT.tsx
@@ -7,25 +7,25 @@ import { ContractLayout } from 'components/contract-pages/contract-layout';
 import { ContractItemsTable } from 'components/contract-pages/table';
 
 export default function MarketplacePage() {
-	// const marketAddress = useSingleQueryParam('marketplace');
-	const marketAddress = '0xc4B0D95D6938E8c154d6637d6586e50B5ee1F44A';
-	const contract = useMarketplace(marketAddress);
-	const metadata = useContractMetadata(contract);
+  // const marketAddress = useSingleQueryParam('marketplace');
+  const marketAddress = '0xc4B0D95D6938E8c154d6637d6586e50B5ee1F44A';
+  const contract = useMarketplace(marketAddress);
+  const metadata = useContractMetadata(contract);
 
-	return (
-		<Providers>
-			<ContractLayout
-				contract={contract}
-				metadata={metadata}
-				primaryAction={<ListButton contract={contract} />}
-			>
-				<ContractItemsTable
-					contract={contract}
-					emptyState={{
-						title: 'You have not created any listings yet.',
-					}}
-				/>
-			</ContractLayout>
-		</Providers>
-	);
+  return (
+    <Providers>
+      <ContractLayout
+        contract={contract}
+        metadata={metadata}
+        primaryAction={<ListButton contract={contract} />}
+      >
+        <ContractItemsTable
+          contract={contract}
+          emptyState={{
+            title: 'You have not created any listings yet.',
+          }}
+        />
+      </ContractLayout>
+    </Providers>
+  );
 }
