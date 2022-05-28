f9d6bf24eb5ec0371caed35c6f12e79671cc4f97
HA <>
2022-05-28 14:07:48 +0800

    refactor: code format

--- pages/listings.tsx
+++ pages/listings.tsx
@@ -1,27 +1,26 @@
-import { useMarketplace } from '@thirdweb-dev/react';
-import { useEffect } from 'react';
+import { useMarketplace } from "@thirdweb-dev/react";
+import { useEffect } from "react";
 
 export default function Component() {
-	const marketplace = useMarketplace(
-		'0xc4B0D95D6938E8c154d6637d6586e50B5ee1F44A',
-	);
+  const marketplace = useMarketplace(
+    "0xEDBEFed02BD700DC0A2149F399c4110abaad8F46"
+  );
 
-	useEffect(() => {
-		const getListings = async () => {
-			try {
-				console.log('marketplace: ', marketplace);
-				const listings = await marketplace?.getActiveListings({
-					seller: '0x01AbECbEB70f67163a3aC8543E88d9C234A71Fa6',
-					tokenContract: '0x720486Cd671E7bdc2935CfcDb2ee43CDBe70B7C5',
-				});
-				// const listings = await marketplace?.getAllListings();
+  useEffect(() => {
+    const getListings = async () => {
+      try {
+        console.log("marketplace: ", marketplace);
+        const listings = await marketplace?.getActiveListings({
+          seller: "0x01AbECbEB70f67163a3aC8543E88d9C234A71Fa6",
+        });
+        // const listings = await marketplace?.getAllListings();
 
-				console.log('listings: ', listings);
-				// const priceOfFirstActiveListing = listings[0].price;
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		getListings();
-	}, []);
+        console.log("listings: ", listings);
+        // const priceOfFirstActiveListing = listings[0].price;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getListings();
+  }, []);
 }
