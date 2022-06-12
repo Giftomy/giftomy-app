f1ca745bf14a27c1de77a897873c5ca1d5a238f5
HA <>
2022-06-12 11:38:37 +0800

    hide self deployed contracts

--- pages/contracts/index.tsx
+++ pages/contracts/index.tsx
@@ -23,8 +23,6 @@ const ContractsHomepageWrapped: React.FC = () => {
     page: "contracts",
   });
 
-  const walletAddress = useAddress();
-  const publishedContracts = usePublishedContractsQuery();
   const TYPES = ["nft-drop", "edition-drop"];
 
   return (
@@ -40,75 +38,6 @@ const ContractsHomepageWrapped: React.FC = () => {
           hasDescription
           contractIds={TYPES}
         />
-        <Box />
-        <Flex gap={2} direction="column">
-          <Heading size="title.md">
-            Your published contracts{" "}
-            <Badge variant="outline" colorScheme="purple">
-              beta
-            </Badge>
-          </Heading>
-          <Text fontStyle="italic">
-            Contracts that you have published via the thirdweb cli
-          </Text>
-        </Flex>
-        <DeployableContractTable
-          isFetching={publishedContracts.isFetching}
-          contractIds={(publishedContracts.data || [])?.map((d) =>
-            d.metadataUri.replace("ipfs://", ""),
-          )}
-        >
-          {publishedContracts.isLoading && (
-            <Center>
-              <Flex mt={4} py={4} direction="row" gap={4} align="center">
-                {walletAddress && <Spinner size="sm" />}
-                <Text>
-                  {walletAddress
-                    ? "Loading your contracts"
-                    : "No wallet connected"}
-                </Text>
-              </Flex>
-            </Center>
-          )}
-          {publishedContracts.isError && (
-            <Center>
-              <Flex mt={4} py={4} direction="column" gap={4} align="center">
-                <Alert status="error" borderRadius="md">
-                  <AlertIcon />
-                  <AlertTitle mr={2}>
-                    Failed to fetch published contracts
-                  </AlertTitle>
-                  <Button
-                    onClick={() => publishedContracts.refetch()}
-                    leftIcon={<IoRefreshSharp />}
-                    ml="auto"
-                    size="sm"
-                    colorScheme="red"
-                  >
-                    Retry
-                  </Button>
-                </Alert>
-              </Flex>
-            </Center>
-          )}
-          {publishedContracts.isSuccess &&
-            publishedContracts.data.length === 0 && (
-              <Center>
-                <Flex mt={4} py={4} direction="column" gap={4} align="center">
-                  <Text>You have not published any contracts yet.</Text>
-                  <LinkButton
-                    size="sm"
-                    href="https://portal.thirdweb.com/thirdweb-deploy"
-                    isExternal
-                    variant="outline"
-                    colorScheme="primary"
-                  >
-                    Learn more
-                  </LinkButton>
-                </Flex>
-              </Center>
-            )}
-        </DeployableContractTable>
       </Flex>
     </Track>
   );
