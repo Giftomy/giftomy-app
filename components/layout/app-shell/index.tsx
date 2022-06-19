5c1cfb01b95d354ba32dbf8f6fc9b6e73665cd12
HA <>
2022-06-19 16:29:42 +0800

    refactor app layout

--- components/layout/app-shell/index.tsx
+++ components/layout/app-shell/index.tsx
@@ -40,13 +40,6 @@ export const AppShell: ComponentWithChildren = ({ children }) => {
 			overflow='hidden'
 			backgroundColor='backgroundBody'
 		>
-			<NextSeo
-				title='Dashboard'
-				openGraph={{
-					title: 'Dashboard | thirdweb',
-					url: `https://thirdweb.com/dashboard`,
-				}}
-			/>
 			<Flex
 				transition='margin 350ms ease'
 				zIndex='docked'
@@ -56,122 +49,8 @@ export const AppShell: ComponentWithChildren = ({ children }) => {
 				flexDir='column'
 				overflowY='auto'
 				id='tw-scroll-container'
+				pt="100px"
 			>
-				<Box
-					background='backgroundHighlight'
-					zIndex='banner'
-					shadow={isCustomContractLayout ? undefined : 'sm'}
-					position={isCustomContractLayout ? undefined : 'sticky'}
-					top={isCustomContractLayout ? undefined : 0}
-				>
-					<Container
-						maxW='container.page'
-						display='flex'
-						py={2}
-						as='header'
-						alignItems='center'
-					>
-						<Link href='/dashboard'>
-							<Logo />
-						</Link>
-						<Stack
-							direction='row'
-							align='center'
-							spacing={{ base: 3, md: 4 }}
-							marginLeft='auto'
-						>
-							<TrackedLink
-								href='https://portal.thirdweb.com/'
-								isExternal
-								variant='link'
-								color='inherit'
-								fontWeight='inherit'
-								textDecoration={undefined}
-								display={{ base: 'none', md: 'block' }}
-								category='header'
-								label='docs'
-							>
-								Docs
-							</TrackedLink>
-							<ButtonGroup
-								variant='ghost'
-								display={{ base: 'none', md: 'block' }}
-							>
-								<TrackedIconButton
-									as={LinkButton}
-									isExternal
-									noIcon
-									href='https://twitter.com/thirdweb_'
-									bg='transparent'
-									aria-label='twitter'
-									icon={
-										<Icon boxSize='1rem' as={SiTwitter} />
-									}
-									category='header'
-									label='twitter'
-								/>
-								<TrackedIconButton
-									as={LinkButton}
-									isExternal
-									noIcon
-									href='https://discord.gg/thirdweb'
-									bg='transparent'
-									aria-label='discord'
-									icon={
-										<Icon boxSize='1rem' as={SiDiscord} />
-									}
-									category='header'
-									label='discord'
-								/>
-								<TrackedIconButton
-									as={LinkButton}
-									isExternal
-									noIcon
-									href='https://www.youtube.com/channel/UCdzMx7Zhy5va5End1-XJFbA'
-									bg='transparent'
-									aria-label='YouTube'
-									icon={
-										<Icon boxSize='1rem' as={SiYoutube} />
-									}
-									category='header'
-									label='youtube'
-								/>
-								<TrackedIconButton
-									as={LinkButton}
-									isExternal
-									noIcon
-									href='https://github.com/thirdweb-dev'
-									bg='transparent'
-									aria-label='github'
-									icon={<Icon boxSize='1rem' as={SiGithub} />}
-									category='header'
-									label='github'
-								/>
-
-								<TrackedIconButton
-									as={LinkButton}
-									noIcon
-									href='/gas'
-									bg='transparent'
-									aria-label='gas-estimator'
-									icon={
-										<Icon
-											boxSize='1rem'
-											as={RiGasStationFill}
-										/>
-									}
-									category='header'
-									label='gas-estimator'
-								/>
-							</ButtonGroup>
-							<ColorModeToggle />
-							<ConnectWallet
-								borderRadius='full'
-								colorScheme='primary'
-							/>
-						</Stack>
-					</Container>
-				</Box>
 				{isCustomContractLayout ? (
 					<Box as='main' flexGrow={1}>
 						{children}
@@ -187,45 +66,6 @@ export const AppShell: ComponentWithChildren = ({ children }) => {
 						{children}
 					</Container>
 				)}
-				<Divider />
-				<Container as='footer' maxW='container.page' w='100%' py={4}>
-					<Stack>
-						<Stack
-							direction='row'
-							spacing='4'
-							align='center'
-							justify='center'
-						>
-							<Text alignSelf='center'>
-								thirdweb &copy; {new Date().getFullYear()}
-							</Text>
-							<TrackedLink
-								isExternal
-								href='https://feedback.thirdweb.com'
-								category='footer'
-								label='feedback'
-							>
-								Feedback
-							</TrackedLink>
-							<TrackedLink
-								isExternal
-								href='/privacy'
-								category='footer'
-								label='privacy'
-							>
-								Privacy Policy
-							</TrackedLink>
-							<TrackedLink
-								isExternal
-								href='/tos'
-								category='footer'
-								label='terms'
-							>
-								Terms of Service
-							</TrackedLink>
-						</Stack>
-					</Stack>
-				</Container>
 				{pathname !== '/dashboard' && (
 					<>
 						<InsufficientFunds />
