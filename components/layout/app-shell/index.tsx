9789dca023f0da782b15155f4d9acaeb7e2b6062
HA <>
2022-06-19 16:35:19 +0800

    refactor: delete unused code

--- components/layout/app-shell/index.tsx
+++ components/layout/app-shell/index.tsx
@@ -1,77 +1,41 @@
-import {
-	Box,
-	ButtonGroup,
-	Container,
-	Divider,
-	Flex,
-	Icon,
-	Stack,
-} from '@chakra-ui/react';
-import { NextSeo } from 'next-seo';
+import { Breadcrumbs } from "./Breadcrumbs";
+import { Container, Flex } from "@chakra-ui/react";
+import { InsufficientFunds } from "components/notices/InsufficientFunds";
 import { useRouter } from 'next/router';
-import React from 'react';
-import { RiGasStationFill } from 'react-icons/ri';
-import { SiDiscord, SiGithub, SiTwitter, SiYoutube } from 'react-icons/si';
-import { InsufficientFunds } from 'components/notices/InsufficientFunds';
-import { Logo } from 'components/logo';
-import { ColorModeToggle } from 'components/color-mode/color-mode-toggle';
-import { ConnectWallet } from '@3rdweb-sdk/react';
-import { Breadcrumbs } from './Breadcrumbs';
-import {
-	Link,
-	LinkButton,
-	Text,
-	TrackedIconButton,
-	TrackedLink,
-} from 'tw-components';
+import React from "react";
 import { ComponentWithChildren } from 'types/component-with-children';
 
 export const AppShell: ComponentWithChildren = ({ children }) => {
 	const { pathname } = useRouter();
-
-	const isCustomContractLayout =
-		pathname === '/[wallet]/[network]/[...customContract]';
-
-	return (
-		<Flex
-			h='calc(100vh - env(safe-area-inset-top) - env(safe-area-inset-bottom))'
-			w='calc(100vw - env(safe-area-inset-left) - env(safe-area-inset-right))'
-			position='relative'
-			overflow='hidden'
-			backgroundColor='backgroundBody'
-		>
-			<Flex
-				transition='margin 350ms ease'
-				zIndex='docked'
-				width='100%'
-				flexGrow={1}
-				flexShrink={0}
-				flexDir='column'
-				overflowY='auto'
-				id='tw-scroll-container'
-				pt="100px"
-			>
-				{isCustomContractLayout ? (
-					<Box as='main' flexGrow={1}>
-						{children}
-					</Box>
-				) : (
-					<Container
-						flexGrow={1}
-						as='main'
-						maxW='container.page'
-						py={8}
-					>
-						<Breadcrumbs />
-						{children}
-					</Container>
-				)}
-				{pathname !== '/dashboard' && (
-					<>
-						<InsufficientFunds />
-					</>
-				)}
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex
+      h="calc(100vh - env(safe-area-inset-top) - env(safe-area-inset-bottom))"
+      w="calc(100vw - env(safe-area-inset-left) - env(safe-area-inset-right))"
+      position="relative"
+      overflow="hidden"
+      backgroundColor="backgroundBody"
+    >
+      <Flex
+        transition="margin 350ms ease"
+        zIndex="docked"
+        width="100%"
+        flexGrow={1}
+        flexShrink={0}
+        flexDir="column"
+        overflowY="auto"
+        id="tw-scroll-container"
+        pt="100px"
+      >
+				<Container flexGrow={1} as="main" maxW="container.page" py={8}>
+					<Breadcrumbs />
+					{children}
+				</Container>
+        {pathname !== "/dashboard" && (
+          <>
+            <InsufficientFunds />
+          </>
+        )}
+      </Flex>
+    </Flex>
+  );
 };
