8da573dbc44843b6bd78ce9ab81f696d4283ab9e
HA <>
2022-06-20 22:10:08 +0800

    refactor: delete unused code

--- src/components/views/homepage/HomeIndex.tsx
+++ src/components/views/homepage/HomeIndex.tsx
@@ -1,15 +1,10 @@
-import HomeChangeMakers from './HomeChangeMakers';
 import HomeExploreProjects from './HomeExploreProjects';
-import HomeFromBlog from './HomeFromBlog';
-import HomeGetUpdates from './HomeGetUpdates';
 import HomeHeader from './HomeHeader';
-import HomePurpleSection from './HomePurpleSection';
 import { IProject } from '@/apollo/types/types';
 import ProjectCard from '@/components/project-card/ProjectCard';
 import NFTDrop from '@/components/views/embeds/nft-drop';
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { Providers } from 'components/app-layouts/providers';
-
 
 interface IHomeView {
   projects: IProject[];
@@ -19,29 +14,21 @@ interface IHomeView {
 const projectsSlice = 6;
 
 const HomeIndex = (props: IHomeView) => {
-	const { projects, totalCount } = props;
-	return (
-		<Providers>
-      <Flex align="center" w="100%" py="85px" px="30px">
-				<ProjectCard key={projects[0].id} project={projects[0]} />
-				{/* TODO */}
-				<NFTDrop contractAddress='0x2732d8e5199B5AB424732c0634f8dF5562Cf37f0' />
-			</Flex>
-			<HomeHeader />
-			<HomeExploreProjects
-				totalCount={totalCount}
-				projects={projects.slice(0, projectsSlice)}
-			/>
-			{/* <HomePurpleSection />
-			<HomeExploreProjects
-				projects={projects.slice(projectsSlice)}
-				noTitle
-			/>
-			<HomeChangeMakers />
-			<HomeFromBlog />
-			<HomeGetUpdates /> */}
-		</Providers>
-	);
+  const { projects, totalCount } = props;
+  return (
+    <Providers>
+      <Flex align='center' w='100%' py='85px' px='30px'>
+        <ProjectCard key={projects[0].id} project={projects[0]} />
+        {/* TODO */}
+        <NFTDrop contractAddress='0x2732d8e5199B5AB424732c0634f8dF5562Cf37f0' />
+      </Flex>
+      <HomeHeader />
+      <HomeExploreProjects
+        totalCount={totalCount}
+        projects={projects.slice(0, projectsSlice)}
+      />
+    </Providers>
+  );
 };
 
 export default HomeIndex;
